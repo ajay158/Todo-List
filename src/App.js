ffdc38ed75523b80444e3b240f3e457d2200ce21
Ajay kumar shahu <1905922@kiit.ac.in>
2022-06-06 22:48:23 +0530

    Todo-List

--- src/App.js
+++ src/App.js
@@ -5,15 +5,15 @@ import TodoList from './Components/TodoList'
 
 
 function App() {
-  const [inputText, setInputText] = useState(" ");
-  const [todo, setTodo] = useState([]);
+  const [inputText, setInputText] = useState(" ");  //setInputText is a function that update the inputText.
+  const [todo, setTodo] = useState([]);  // setTodo is a function that update the todo.
   return (
     <div className="App">
       <header>
         <h1>Ajay's TODO-List </h1>
       </header>
-      <Form inputText={inputText}todo={todo} setTodo={setTodo} setInputText={setInputText}/>
-      <TodoList />
+      <Form inputText={inputText} todo={todo} setTodo={setTodo} setInputText={setInputText} />
+      <TodoList todo={todo} />
     </div>
   );
 }
--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,33 +1,35 @@
 import React from 'react'
 
-function Form({setInputText, todo, setTodo, inputText}) {
-    const inputTextHandler = (e) =>{
-        console.log(e.target.value);
-        setInputText(e.target.value)
+function Form({ setInputText, todo, setTodo, inputText }) {
+    const inputTextHandler = (e) => {
+        console.log(e.target.value);  // we use this because all the changes are seen when user click in that input.
+        setInputText(e.target.value)  // we use this because all the changes are seen where user click on submit button.
     }
 
-    const submitTodoHandler = (e) =>{
+    const submitTodoHandler = (e) => {
         e.preventDefault();
         setTodo([
-            ...todo, {inputText, inputText, completed: false, id: Math.random() *  1000},
-            ])
+            ...todo, { text: inputText, completed: false, id: Math.random() * 1000 },
+        ])  // ()...todo) mean that if i had  todo already in list just pass it.
+        setInputText("");
     }
 
-    return (
-        <form>
-            <input onChange={inputTextHandler} type="text" className="todo-input" />
-            <button onClick={submitTodoHandler} className="todo-button" type="submit">
-                <i className="fas fa-plus-square"></i>
-            </button>
-            <div className="select">
-                <select name="todos" className="filter-todo">
-                    <option value="all">All</option>
-                    <option value="completed">Completed</option>
-                    <option value="uncompleted">Uncompleted</option>
-                </select>
-            </div>
-        </form>
-    )
+    
+return (
+    <form>
+        <input value={inputText} onChange={inputTextHandler} type="text" className="todo-input" />
+        <button onClick={submitTodoHandler} className="todo-button" type="submit">
+            <i className="fas fa-plus-square"></i>
+        </button>
+        <div className="select">
+            <select name="todos" className="filter-todo">
+                <option value="all">All</option>
+                <option value="completed">Completed</option>
+                <option value="uncompleted">Uncompleted</option>
+            </select>
+        </div>
+    </form>
+)
 }
 
 export default Form
